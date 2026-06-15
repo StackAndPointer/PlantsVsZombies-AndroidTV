22bb3edbcc08ca629997cb719773a2e9c0832056
无敌战神领主 <1276007528@qq.com>
2026-06-15 23:29:17 +0800

    feat: 移动对战的铲子位置

--- app/src/main/cpp/PvZ/include/PvZ/Lawn/Board/Board.h
+++ app/src/main/cpp/PvZ/include/PvZ/Lawn/Board/Board.h
@@ -693,7 +693,7 @@ inline int choiceSeedPacketIndex;
 inline SeedType choiceSeedType = SeedType::SEED_NONE;
 inline bool isImitaterSeed;
 inline bool setSeedPacket;
-inline Sexy::Rect gTouchVSShovelRect = {-50, 90, 70, 72};
+inline Sexy::Rect gTouchVSShovelRect = {-70, 170, 70, 72};
 
 inline bool hideCoverLayer;
 inline bool infiniteSun; // 无限阳光
